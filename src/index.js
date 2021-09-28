4f2cacb63f6ea768654f361e7e24cfc55f097295
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-09-28 02:40:44 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +10,70 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const array = ["Peras", "Manzanas", "Sandias"];
+
+/* const render = (       //DEJAR COMENTADO PARA EL EJERCICIO
+  <ol>
+    <li>{array[0]}</li>
+    <li>{array[1]}</li>
+    <li>{array[2]}</li>
+  </ol>
+); */
+
+// const TituloFrutas = () => {
+//   return <h1>Frutas:</h1>
+// }
+
+// const Subtitulo = () => <h4>Título 2</h4>;
+
+// const Parrafo = () => <p></p>;
+
+// const Lista = () => {
+//   return (
+//     <ol>
+//         <li>{array[0]}</li>
+//         <li>{array[1]}</li>
+//         <li>{array[2]}</li>
+//       </ol>
+//   );
+// };
+
+// const Frutas = () => {
+//   return (
+//     <>
+//       <TituloFrutas />
+//       <Subtitulo />
+//       <Parrafo />
+//       <Lista />     
+// 	  </>
+//   );
+// };
+
+// ReactDOM.render(
+// 	<Frutas />, 
+// 	document.getElementById("root")
+// );
+
+// Ejercicio - RESUELTO. Ver código de arriba
+// 1.-Modificar el código de arriba y crear un componente que se llame "subtitulo" y que acepte el h4 "Titulo 2"
+// 2.- Luego, generar un componente llamado "parrafo" que dentro lleve un párrafo.
+// 3.- Generar un último componente que contenga la ol con sus items.
+
+
+// ==============================================================================================================
+// PARTE 2 DE LA CLASE"
+
+
+
+
+
+
+
