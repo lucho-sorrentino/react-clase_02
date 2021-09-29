3b5237e74a484308b23db5bc9faa6f4781f9df38
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-09-29 02:05:11 -0300

    Código de la clase terminado

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.render(
 
 // const Subtitulo = () => <h4>Título 2</h4>;
 
-// const Parrafo = () => <p></p>;
+// const Parrafo = () => <p>Lorem</p>;
 
 // const Lista = () => {
 //   return (
@@ -42,7 +42,7 @@ ReactDOM.render(
 //         <li>{array[0]}</li>
 //         <li>{array[1]}</li>
 //         <li>{array[2]}</li>
-//       </ol>
+//      </ol>
 //   );
 // };
 
@@ -69,10 +69,67 @@ ReactDOM.render(
 
 
 // ==============================================================================================================
-// PARTE 2 DE LA CLASE"
+// PARTE 2 DE LA CLASE: a partir de 2:34:10
 
+import React from "react";
+import ReactDOM from "react-dom";
 
+const array = ["Peras", "Manzanas", "Sandias"];
 
+const render = (
+  <ol>
+    <li>{array[0]}</li>
+    <li>{array[1]}</li>
+    <li>{array[2]}</li>
+  </ol>
+);
+
+const TituloFrutas = (props) => {
+  const { titulo, id } = props;
+  return (
+    <>
+      <h1>{titulo}</h1>
+      <p>El id es: {id}</p>
+    </>
+  );
+};
+
+const Subtitulo = ({ sub, num }) => <h4>{num * 2}</h4>;
+
+const Parrafo = () => <p>Lorem solo</p>;
+
+const Lista = () => (
+  <ol>
+    <li>{array[0]}</li>
+    <li>{array[1]}</li>
+    <li>{array[2]}</li>
+  </ol>
+);
+
+const Frutas = () => {
+  return (
+    <>
+      <TituloFrutas titulo="Un titulo para Frutas" id={1} />
+      <Subtitulo sub="Esto es un subtitulo" num={5} />
+      <Parrafo />
+      <Lista />
+    </>
+  );
+};
+
+ReactDOM.render(
+  <Frutas />,
+  /*
+  React.createElement(
+    "ol",
+    null,
+    React.createElement("li", null, array[0]),
+    React.createElement("li", null, array[1]),
+    React.createElement("li", null, array[2])
+  )*/ document.getElementById(
+    "root"
+  )
+);
 
 
 
